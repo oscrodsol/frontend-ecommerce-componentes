1b357296aaa6c84634873ef5e22c985ad111d995
oscrodsol <oscar43521@gmail.com>
2022-08-23 19:05:09 +0200

    feat: Created base Login and Register

---
+++ src/Containers/User/Login/Login.jsx
@@ -0,0 +1,84 @@
+import React, {useEffect} from 'react';
+import './Login.css';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectDatosUsuario, loginUsuario } from '../userSlice';
+import { useState } from 'react';
+import img3 from '../../../assets/iconoform.png'
+
+const Login = (props) => {
+    //hooks
+    const [datosLogin, setDatosLogin] = useState ({
+        dni:'', 
+        password:''
+    })  
+    const [msgError, setMsgError] = useState("");
+
+    //Variables
+    let navegador = useNavigate();
+    //Dispatch va a ser un metodo necesario de redux que vamos a usar
+    const dispatch = useDispatch();
+    const credenciales = useSelector(selectDatosUsuario)
+
+    //Handlers
+    const modificaDatosLogin = (e) => {
+        setDatosLogin({
+            ...datosLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect (()=>{
+        //Comprobamos si tenemos token
+        if(credenciales?.token !== ''){
+            navegador("/Profile");
+        }
+    },[]);
+
+    const logeame = () => {
+        //Primero compruebo que los campos sean correctos
+            //Esta expresión regular ayuda a validar un email
+        if (! /[0-9]{7,8}[A-Z]/g.test(datosLogin.dni) ) {
+            setMsgError('Introduce un dni válido');
+            return;
+        }
+            //Esta expresión regular ayuda a validar un password (numero + letras en este caso)
+        if(datosLogin.password.length > 2){
+            
+            if (! /[\d()+-]/g.test(datosLogin.password) ) {
+                setMsgError('Introduce un password válido');
+                return;
+            };
+        }else{
+            setMsgError('El password debe de tener como mínimo 4 caracteres');
+            return;
+        }
+        //Por si acaso teníamos algo referenciado como error, lo limpiamos
+        setMsgError("");
+        //Dispatch es el método de redux que ejecuta el reducer
+        dispatch(loginUsuario({dni: datosLogin.dni,
+            password: datosLogin.password}
+        ));
+
+        setTimeout(()=>{
+            navegador("/");
+        },1000)
+    };
+
+    return(
+            <div className="login">
+                <div className='loginDesign'>
+                    <h1>Login</h1>
+                    {/* <pre>{JSON.stringify(datosLogin, null,2)}</pre> */}
+                    <input  type='email' name='dni' title='dni' onChange={modificaDatosLogin} lenght='30'/>
+                    <input  type='password'  name='password' title='password' onChange={modificaDatosLogin} lenght='30'/><br></br>
+                    <div className="msgError">{msgError}</div>
+                    <div className="sendButton" onClick={()=>logeame()}>Login</div><br></br>
+                    <div className="botonregister" to="/register">Si no estas registrado, registrate aqui.<img className="iregister "src={img3} onClick={()=> navegador("/register")}/></div>
+                    
+                </div>
+            </div>
+    )
+}
+
+export default Login
---
+++ src/Containers/User/Register/Register.jsx
@@ -0,0 +1,113 @@
+
+import React from "react"
+import { useEffect, useState } from "react"
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { registerUser, userSelector } from '../userSlice'
+import { useNavigate } from 'react-router-dom'
+import "./Register.css"
+
+const Register = props => {
+    const dispatch = useDispatch()
+    let navigate = useNavigate()
+    const userData = useSelector(userSelector)
+    const [msgError, setMsgError] = useState("");
+
+    const [register, setRegister] = useState({
+        nombre: '',
+        dni: '',
+        password: '',
+        email: '',
+        telefono: ''
+        
+    })
+
+
+    const handleInput = (event) => {
+        setRegister({
+            ...register,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const userRegister = (event) => {
+        event.preventDefault()
+
+        if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(register.email)) {
+            setRegister({
+                ...register,
+                isError: true,
+                message: 'Introduce un e-mail válido'
+            });
+            return;
+        }
+
+        if (register.password.length > 1) {
+            if (! /[\d()+-]/g.test(register.password)) {
+                setRegister({
+                    ...register,
+                    isError: true,
+                    message: 'Introduce un password válido'
+                });
+                return;
+            };
+
+        } else {
+            setRegister({
+                ...register,
+                isError: true,
+                message: 'El password debe de tener como mínimo 4 caracteres'
+            });
+            return;
+        }
+
+        setRegister({
+            ...register,
+            isError: false,
+            errorMsg: ''
+        });
+
+        dispatch(registerUser(register.nombre, register.dni, register.password, register.email, register.telefono))
+
+    }
+
+    return (
+        <Row className="Register justify-content-md-center">
+            <Col md={6}>
+                <h1>Registro</h1>
+                <br></br>
+                 {/* <pre>{JSON.stringify(register, null,5)}</pre> */} 
+                <Form onSubmit={userRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="text" name="nombre" onChange={handleInput} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>DNI</Form.Label>
+                        <Form.Control type="text" name="dni" onChange={handleInput} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control type="password" name="password" onChange={handleInput} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" name="email" onChange={handleInput} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Teléfono</Form.Label>
+                        <Form.Control type="text" name="telefono" onChange={handleInput} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Registrarse
+                    </Button>
+                </Form>
+                {/*ponerme el error bonito!*/}
+{/*                 <p>{msgError}</p>
+                <p>{msgError ? msgError : userData.successMessage}</p> */}
+            </Col>
+        </Row>
+    )
+}
+
+export default Register
